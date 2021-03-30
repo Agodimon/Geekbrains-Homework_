4a3d8fb951e1093800ac84594c3125d45fe2befb
Agodimon <Agodimon@mail.ru>
2021-03-30 16:13:56 +0500

    По ДЗ №4 выполнил по методички с 1 по 8 пункт. Осталось написать метод реализации победы.

--- src/com/company/lesson4/Main.java
+++ src/com/company/lesson4/Main.java
@@ -1,16 +1,27 @@
 package com.company.lesson4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static char[][] map;
     public static final int SIZE=3;
+
     public static final int DITS_TO_WIN=3;
+
     public static final char DOT_EMPTY='*';
-    public static final char X='X';
-    public static final char O='O';
+    public static final char DOT_X='X';
+    public static final char DOT_O='O';
+
+    public static Scanner scanner=new Scanner(System.in);
+    public static Random    rand = new Random();
 
     public static void main(String[] args) {
     initMap();
     printMap();
+
+    scanner.close();
     }
 
     public static void initMap() {
@@ -23,18 +34,42 @@ public class Main {
     }
 
     public static void printMap() {
-for (int i=0;i<SIZE;i++){
-    System.out.print(i+" ");
-}
-        System.out.println();
-for (int i=0;i<SIZE;i++){
-    System.out.print((i+1)+" ");
-    for(int j=0;j<SIZE;j++){
-        System.out.print(map[i][j] + " ");
+        for (int i=0;i<=SIZE;i++){
+            System.out.print(i+" ");
+        }
+            System.out.println();
+        for (int i=0;i<SIZE;i++){
+            System.out.print((i+1)+" ");
+            for(int j=0;j<SIZE;j++){
+                System.out.print(map[i][j] + " ");
+            }
+                System.out.println();
+        }
+                System.out.println();
     }
-    System.out.println();
-}
-        System.out.println();
+
+    public static void humanTurn() {
+int x,y;
+do {
+    System.out.println("Введите координате в формате X Y");
+    x=scanner.nextInt()-1;
+    y=scanner.nextInt()-1;
+}while(isNotCellValid(x, y));
+map[y][x]=DOT_X;
+    }
+
+    public static boolean isNotCellValid(int x, int y) {
+    if (x<0||x>=SIZE||y<0||y>=SIZE) return true;
+        return map[y][x] != DOT_EMPTY;
+    }
+    public static void aiTurn(){
+    int x, y;
+    do{
+        x= rand.nextInt(SIZE);
+        y= rand.nextInt(SIZE);
+    }while (isNotCellValid(x, y));
+        System.out.println("Компьютер походил в точку "+(x+1)+" "+(y+1));
+        map[y][x]=DOT_O;
     }
 
 }
