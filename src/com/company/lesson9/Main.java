9f6099dc682d96318b4c43e35bc53d12c3094808
Agodimon <Agodimon@mail.ru>
2021-04-18 16:50:52 +0500

    Делаю дз №9. Ввод двумерного массива, преобразование массивов в числа.

--- src/com/company/lesson9/Main.java
+++ src/com/company/lesson9/Main.java
@@ -2,15 +2,26 @@ package com.company.lesson9;
 
 public class Main {
     public static void main(String args[]) {
-        int counter=1;
-        String[][] table=new String[4][4];
-        for (int i=0;i<table.length;i++){
-            for (int j=0;j<table.length;j++){
-                int s = Integer.parseInt(table[i][j].trim());
-            }
 
+        String[][] array={{" 2"," 4"," 9","10"},{"44","89","77","87"},{"33","12","11","7"},{"10","88","55","33"}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+
+                int s = Integer.parseInt(array[i][j].trim());
+                System.out.print(" "+s+" ");
+            }
+            System.out.println();
         }
 
+        //convertIndexArrayStringToInt(table);
+    }
 
+    private static void convertIndexArrayStringToInt(String[][] table) {
+        for (int i = 0; i< table.length; i++){
+            for (int j = 0; j< table.length; j++){
+                int s = Integer.parseInt(table[i][j].trim());
+                System.out.println(s);
+            }
+        }
     }
 }
