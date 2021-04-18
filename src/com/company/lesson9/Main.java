577d1e1ee4c6c7d4d7a61081b1975ae8e03c9485
Agodimon <Agodimon@mail.ru>
2021-04-18 15:50:16 +0500

    Делаю дз №9.

--- src/com/company/lesson9/Main.java
+++ src/com/company/lesson9/Main.java
@@ -1,4 +1,16 @@
 package com.company.lesson9;
 
 public class Main {
+    public static void main(String args[]) {
+        int counter=1;
+        String[][] table=new String[4][4];
+        for (int i=0;i<table.length;i++){
+            for (int j=0;j<table.length;j++){
+                int s = Integer.parseInt(table[i][j].trim());
+            }
+
+        }
+
+
+    }
 }
---
+++ src/com/company/lesson9/MyArrayDataException.java
@@ -0,0 +1,4 @@
+package com.company.lesson9;
+
+public class MyArrayDataException extends NumberFormatException{
+}
