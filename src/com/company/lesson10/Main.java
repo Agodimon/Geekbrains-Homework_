176f06ce74d0af0f1c58e33b500ee5ef22b493dc
Agodimon <Agodimon@mail.ru>
2021-04-22 20:33:34 +0500

    Doing my homework number ten (practicLesson10). Completed task one.

---
+++ src/com/company/lesson10/Main.java
@@ -0,0 +1,31 @@
+package com.company.lesson10;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        int count=0;
+        Set<String> set=new HashSet<>();
+        set.add("Dog");
+        set.add("Cat");
+        set.add("Worm");
+        set.add("Pig");
+        set.add("Tiger");
+        set.add("Pig");
+        set.add("Chicken");
+        set.add("Horse");
+        set.add("Sloth");
+        set.add("Cat");
+        Iterator<String> iter= set.iterator();
+        while (iter.hasNext()){
+            String str= iter.next();
+            count++;
+        }
+        System.out.println("List of unique words: "+set);
+        System.out.println("Counting unique words = "+ count);
+
+    }
+}
