9762c20b89f76cf439739ec113847365e427bb78
Agodimon <Agodimon@mail.ru>
2021-04-23 17:34:38 +0500

    Finished task №1 of homework lesson 10.

--- src/com/company/lesson10/Main.java
+++ src/com/company/lesson10/Main.java
@@ -1,33 +1,21 @@
 package com.company.lesson10;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        outputOfUniqueWordsFromAnArrayOfWordsAndCounting();
+
+    }
+
+    private static void outputOfUniqueWordsFromAnArrayOfWordsAndCounting() {
         int count=0;
-        Set<String> set=new HashSet<>();
-        set.add("Dog");
-        set.add("Cat");
-        set.add("Worm");
-        set.add("Pig");
-        set.add("Tiger");
-        set.add("Pig");
-        set.add("Chicken");
-        set.add("Horse");
-        set.add("Sloth");
-        set.add("Cat");
-        Iterator<String> iter= set.iterator();
-        while (iter.hasNext()){
-            iter.next();
+        String[] arraysOfWord =new String[]{"Dog","Cat","Worm","Chicken","Pig","Cat","Alligator","Worm","Cat","Pig","Tiger","Cat","Chicken","Horse","Sloth","Horse"};
+        Set<String> arraysSet = new HashSet<>(Arrays.asList(arraysOfWord));
+        for (String ignored : arraysSet) {
             count++;
-
         }
-        System.out.println("List of unique words: "+set);
+        System.out.println("List of unique words: "+arraysSet);
         System.out.println("Counting unique words = "+ count);
-
-
     }
 }
