f28099c9cc48c6605bbae181b69bd6a5e4eb58f2
Agodimon <Agodimon@mail.ru>
2021-04-25 23:01:29 +0500

    Complete homework №10 task one and task two.

---
+++ src/com/company/lesson11/Box.java
@@ -0,0 +1,8 @@
+package com.company.lesson11;
+
+
+import java.util.ArrayList;
+
+public class Box <T extends Fruit>{
+    private ArrayList<T> box=new ArrayList<>();
+}
--- src/com/company/lesson11/Main.java
+++ src/com/company/lesson11/Main.java
@@ -1,26 +1,43 @@
 package com.company.lesson11;
 
-import jdk.jfr.internal.Type;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-    BoxArr<Integer> arraysInt=new BoxArr<>(1,2,3,4,6,8,9);
-      arraysInt.getArray();
+        playingTaskNumberOneAndTwoLessonOfPracticTenExampleTypeInteger();
 
-        arraysInt.changingElemArray(0,3);
+        playingTaskNumberOneAndTwoLessonOfPracticTenExampleTypeString();
 
-        arraysInt.getArray();
-        System.out.println(arraysInt.arrToArrList());
-        BoxArr<String> arrString=new BoxArr<>("1","3","fishhook","$$#");
+
+    }
+
+    private static void playingTaskNumberOneAndTwoLessonOfPracticTenExampleTypeString() {
+        TaskOneAndTwo<String> arrString=new TaskOneAndTwo<>("1","3","fishhook","$$#");
         arrString.getArray();
         arrString.changingElemArray(0,3);
         arrString.getArray();
+        List<String> listOfStr=new ArrayList<>();
+        arrString.arrToArrList(listOfStr);
+        listOfStr.add("addTest");
+        listOfStr.add(2,"testTwo");
+        listOfStr.remove(1);
+        System.out.println(listOfStr);
+    }
 
-
+    private static void playingTaskNumberOneAndTwoLessonOfPracticTenExampleTypeInteger() {
+        TaskOneAndTwo<Integer> arraysInt=new TaskOneAndTwo<>(1,2,3,4,6,8,9);
+        arraysInt.getArray();
+        arraysInt.changingElemArray(0,3);
+        arraysInt.getArray();
+        List<Integer> listOfT =new ArrayList<>();
+        arraysInt.arrToArrList(listOfT);
+        listOfT.add(15);
+        listOfT.add(25);
+        System.out.println(listOfT);
     }
 
 
