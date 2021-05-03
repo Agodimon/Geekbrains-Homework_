0be74255b5e150a901f78a696c013fd5235916da
Agodimon <Agodimon@mail.ru>
2021-05-03 23:18:18 +0500

    complete homework of lesson 13.

--- src/com/company/lesson13/MainClass.java
+++ src/com/company/lesson13/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         CyclicBarrier cb = new CyclicBarrier(5);
         CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(70), new Tunnel(), new Road(25));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cb);
